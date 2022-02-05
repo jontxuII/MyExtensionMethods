36b3faa75cb0f8e0b4c8cb4efa4b6d8940906eed
jontxuII <iberimanol@gmail.com>
2022-02-05 05:06:23 +0100

    Agregar archivos de proyecto.

---
+++ MyExtensionMethods.Console/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace MyExtensionMethods.Test
+{
+    public enum Equipos : int
+    {
+        // Para tener un valor por defecto
+        SinEquipo = 0,
+
+        [Description("San Antonio")]
+        SanAntonio = 1,
+
+        Chicago = 2,
+
+        [Description("L.A. Lakers")]
+        LaLakers = 3,  
+        
+        LaClippers = 4,
+
+        Detroit = 5,
+
+        Dallas = 6, 
+    }
+
+    [Flags]
+    public enum Posiciones : int
+    {
+        uno = 1,
+        dos = 2,
+        tres = 4,
+        cuatro = 8,
+        cinco = 16,
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0}. Desde atributo Description: {1}", Equipos.SanAntonio.GetDescription(), Equipos.SanAntonio.HasDescription());
+            Console.WriteLine("{0}. Desde atributo Description: {1}", Equipos.LaClippers.GetDescription(), Equipos.LaClippers.HasDescription());
+            Console.WriteLine("{0}. Desde atributo Description: {1}", Equipos.LaLakers.GetDescription(), Equipos.LaLakers.HasDescription());
+
+            var magic = Posiciones.uno | Posiciones.dos | Posiciones.tres | Posiciones.cuatro | Posiciones.cinco;
+            var dirk = Posiciones.tres | Posiciones.cuatro | Posiciones.cinco;
+
+            Console.WriteLine(magic.HasFlag(Posiciones.dos));
+            Console.WriteLine(dirk.HasFlag(Posiciones.dos));
+
+            var equipo1 = "Chicago".ToEnum<Equipos>();
+            Equipos? equipo2 = "Toronto".ToEnum<Equipos>();
+
+            Dictionary<string, string> dicEquipos = typeof(Equipos).ToDictionary();
+            Dictionary<string, string> dicPosiciones = typeof(Posiciones).ToDictionary();
+
+            var algo = magic.ParseFromString("Magic", Posiciones.dos);
+            algo = magic.ParseFromString<Posiciones>("Magic");
+            algo = magic.ParseFromString<Posiciones>("Uno");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MyExtensionMethods/EnumExtensions.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace MyExtensionMethods
+{
+    /// <summary>
+    /// Extension methods para enumeradores
+    /// </summary>
+    public static class EnumExtension
+    {
+        /// <summary>
+        /// Obtiene el atributo Description de un enumerador
+        /// </summary>
+        public static string GetDescription(this Enum enumMember)
+        {
+            var attr = GetDescriptionAttribute(enumMember);
+            
+            if (attr != null) return attr.Description;
+
+            return enumMember.ToString();
+        }
+
+        /// <summary>
+        /// Comprueba si un enumerador tiene un determinado Flag
+        /// </summary>
+        public static bool IsSet(this Enum self, Enum valueMemberToCheck)
+        {
+            return (Convert.ToUInt32(self) & Convert.ToUInt32(valueMemberToCheck)) != 0;
+        }
+
+        /// <summary>
+        /// Parsea un valor string a un enumerador. Si no existe, devuelve el valor por defecto del enumerador.
+        /// </summary>
+        public static T? ToEnum<T>(this string value) where T : struct
+        {
+            if (string.IsNullOrEmpty(value)) return default(T);
+            return Enum.TryParse(value, true, out T result) ? result : default;
+        }
+
+        #region HasDescription
+        /// <summary>
+        /// Comprueba si un enumerador tiene el atributo Description
+        /// </summary>
+        public static bool HasDescription(this Enum enumMember)
+        {
+            return GetDescriptionAttribute(enumMember) != null;
+        }
+
+        /// <summary>
+        /// Comprueba si un enumerador tiene el atributo Description
+        /// </summary>
+        public static bool HasDescription(this Enum enumMember, string description)
+        {
+            var attr = GetDescriptionAttribute(enumMember);
+            return attr != null && attr.Description.Equals(description);
+        }
+
+        /// <summary>
+        /// Comprueba si un enumerador tiene el atributo Description
+        /// </summary>
+        public static bool HasDescription(this Enum enumMember, string description, StringComparison comparisionType)
+        {
+            var attr = GetDescriptionAttribute(enumMember);
+            return attr != null && attr.Description.Equals(description, comparisionType);
+        }
+        #endregion HasDescription
+
+        /// <summary>
+        /// Parsea el valor string. Si no existe, devuelve el valor por defecto
+        /// </summary>
+        public static T ParseFromString<T>(this Enum enumMember, string value) where T : struct
+        {
+            return enumMember.ParseFromString(value, default(T));
+        }
+
+        /// <summary>
+        /// Parsea el valor string. Si no existe, devuelve el valor por defecto indicado
+        /// </summary>
+        public static T ParseFromString<T>(this Enum enumMember, string value, T defaultValue = default) where T : struct
+        {
+            if (enumMember is null) throw new ArgumentNullException(nameof(enumMember));
+
+            var done = Enum.TryParse(value, true, out T res);
+
+            return done ? res : defaultValue;
+        }
+
+        /// <summary>
+        /// Obtiene un diccionario con todos los valores del enumerador y su descripción
+        /// </summary>
+        public static Dictionary<string, string> ToDictionary(this Type enumType)
+        {
+            if (enumType.IsEnum)
+            {
+                var rtdo = new Dictionary<string, string>();
+
+                Enum.GetValues(enumType)
+                    .Cast<int>()
+                    .Where(x => !x.Equals(0))
+                    .ToList()
+                    .ForEach(x =>
+                    {
+                        var aux = (Enum)Enum.Parse(enumType, x.ToString());
+                        rtdo.Add(aux.ToString(), aux.GetDescription());
+                    });
+
+                return rtdo;
+            }
+
+            return null;
+        }
+
+        #region private methods
+        /// <summary>
+        /// Obtiene el atributo Description de un enumerador. Null, si no existe
+        /// </summary>
+        private static DescriptionAttribute GetDescriptionAttribute(Enum enumMember)
+        {
+            Type type = enumMember.GetType();
+            MemberInfo[] memInfo = type.GetMember(enumMember.ToString());
+
+            if (memInfo != null && memInfo.Length > 0)
+            {
+                object[] attrs = memInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+                if (attrs != null && attrs.Length > 0)
+                    return (DescriptionAttribute)attrs[0];
+            }
+
+            return null;
+        }
+        #endregion private methods
+    }
+}
